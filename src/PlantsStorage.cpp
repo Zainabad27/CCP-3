2ef5c7d15fb87322f1c6ebf8fbaf6021759cb58c
zainabad <zainabad27@gmail.com>
2026-05-01 23:28:24 +0500

    initial commit.

---
+++ src/PlantsStorage.cpp
@@ -0,0 +1,30 @@
+// the module that keep the track of all the plants and their data and condition.
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+#define PLANTS_HEALTH_LEVELS 10
+
+
+// we basically have to write four main funcs here which are 
+
+
+struct plants
+{
+    string name;
+    int healthLevel;
+    int waterLevel;
+    int sunlightLevel;
+
+};
+
+
+
+void addPlant(plants plant,vector<vector<plants>>& plantStorage)
+{
+    
+
+}
+
+
